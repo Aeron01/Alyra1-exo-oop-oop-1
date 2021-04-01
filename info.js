8171e1a6ea4aae55f265f58ebfa438be7f9e7369
Aeron01 <one.soulreaper@gmail.com>
2021-04-01 15:57:20 +0200

    2nd info.js done

--- info.js
+++ info.js
@@ -1,17 +1,26 @@
 const alice = {
-  firstName='Alice',
-  lastName='Liddell',
-  age=28,
+  firstName: 'Alice',
+  lastName: 'Liddell',
+  age: 28,
 }
 
 const bob = {
-  firstName='Bob',
-  lastName='Lemon',
-  age=30,
+  firstName: 'Bob',
+  lastName: 'Lemon',
+  age: 30,
 }
 
 const charlie = {
-  firstName='Charlie',
-  lastName='Charlot',
-  age=8,
+  firstName: 'Charlie',
+  lastName: 'Charlot',
+  age: 8,
 }
+
+const canVote = (person) => {
+  const majo = 18
+  return person.age < majo ? false : true
+}
+
+console.log(canVote(alice))
+console.log(canVote(bob))
+console.log(canVote(charlie))
